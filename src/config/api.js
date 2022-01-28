b7843ee19fd39fed225ec4d4224d7b61e0aa7ca4
akmalsyrf <dev.akmal69@gmail.com>
2022-01-28 16:13:16 +0700

    fix url

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 export const API = () => {
-  const baseUrl = "http://localhost:5000/api/v1";
+  const baseUrl = process.env.SERVER_URL || "https://backend-dumbmerch.herokuapp.com/api/v1" || "http://localhost:5000/api/v1";
 
   const executeAPI = async (endpoint, config) => {
     const response = await fetch(baseUrl + endpoint, config);
